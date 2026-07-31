4f90582e6989e1266951cbef372050da4cff7d1a
Aditya Yadav <adityaa89468@gmail.com>
2026-07-31 11:40:57 +0530

    feat: migrate to native Capacitor BridgeActivity
    Replaced the manual WebView implementation with `BridgeActivity` to enable native Capacitor functionality. Added `@capacitor/assets` for asset management.

--- android/app/src/main/java/com/adityaproductiontester4tester/app/MainActivity.kt
+++ android/app/src/main/java/com/adityaproductiontester4tester/app/MainActivity.kt
@@ -1,51 +1,6 @@
 package com.adityaproductiontester4tester.app
 
-import android.annotation.SuppressLint
-import android.os.Bundle
-import android.webkit.WebSettings
-import android.webkit.WebView
-import android.webkit.WebViewClient
-import androidx.appcompat.app.AppCompatActivity
+import com.getcapacitor.BridgeActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BridgeActivity()
 
-    private lateinit var webView: WebView
-
-    @SuppressLint("SetJavaScriptEnabled")
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        
-        webView = WebView(this)
-        setContentView(webView)
-
-        webView.settings.apply {
-            javaScriptEnabled = true
-            domStorageEnabled = true
-            databaseEnabled = true
-            useWideViewPort = true
-            loadWithOverviewMode = true
-            mixedContentMode = WebSettings.MIXED_CONTENT_COMPATIBILITY_MODE
-        }
-
-        webView.webViewClient = object : WebViewClient() {
-            @Deprecated("Deprecated in Java", ReplaceWith("shouldOverrideUrlLoading(view, request)"))
-            @Suppress("DEPRECATION")
-            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
-                return false
-            }
-        }
-
-        // Loads the live TesterSetu application engine
-        webView.loadUrl("https://ais-dev-dp6u2vzslmpecx7aovcqg3-309080682455.asia-east1.run.app")
-    }
-
-    @Deprecated("Deprecated in Java")
-    @Suppress("DEPRECATION")
-    override fun onBackPressed() {
-        if (webView.canGoBack()) {
-            webView.goBack()
-        } else {
-            super.onBackPressed()
-        }
-    }
-}
